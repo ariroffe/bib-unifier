6676adafec461d7a7c196ef9f21e3d2a5dfe6d80
ariel <ariroffe@gmail.com>
2022-01-27 11:35:10 -0300

    test bib files, read them and print them to console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use ::std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file1_path = "test_files/test1.bib";
+    let file2_path = "test_files/test2.bib";
+
+    let file1 = fs::read_to_string(file1_path).expect("Could not read file1");
+    println!("FILE 1: {}", file1);
+
+    let file2 = fs::read_to_string(file2_path).expect("Could not read file2");
+    println!("FILE 2: {}", file2);
 }
