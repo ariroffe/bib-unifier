f66bc5f56322ed0ae16516f4f122e24a3ba08c02
ariel <ariroffe@gmail.com>
2022-02-01 12:54:37 -0300

    Nicer error messages to the end user

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::io;
 
+use anyhow::{Context, Result};
 use clap::Parser;
 
 use bib_unifier;
 
-fn main() -> Result<(), io::Error>{
+fn main() -> Result<()>{
     let config = bib_unifier::Config::parse();
 
-    bib_unifier::run(config)?;
+    bib_unifier::run(config).with_context(|| "An error occured and the program was terminated")?;
     Ok(())
 }
